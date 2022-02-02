514500f06361f0f6d7bcce995c20d1190458d68a
Fernando Stteffen <fernando.stteffen@gmail.com>
2022-02-02 17:27:29 -0300

    fix: add @PrimaryColumn to city entity

--- src/modules/cities/entities/City.ts
+++ src/modules/cities/entities/City.ts
@@ -1,9 +1,9 @@
-import { Column, Entity } from "typeorm";
+import { Column, Entity, PrimaryColumn } from "typeorm";
 import { v4 as uuidV4 } from "uuid";
 
 @Entity("cities")
 class City {
-  @Column()
+  @PrimaryColumn()
   id?: string;
 
   @Column()
