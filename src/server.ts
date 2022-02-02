dc430842c6ca29c322444bd2c3fd12b505b1eebd
Fernando Stteffen <fernando.stteffen@gmail.com>
2022-02-02 17:00:17 -0300

    feat: add error tratament to  middleware to server.ts

--- src/server.ts
+++ src/server.ts
@@ -6,12 +6,29 @@ import "./database";
 
 import "@shared/container";
 
+import "@errors/AppError";
+
 const PORT = 3000;
 
 const app = express();
 
 app.use(express.json());
 
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      message: `Internal server error - ${err.message}`,
+    });
+
+    next();
+  }
+);
 app.listen(PORT, () =>
   console.log(`Server as Listen on http://localhost:${PORT}`)
 );
