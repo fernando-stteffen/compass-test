f257f50d0a243831f39c534956495460875bfd55
Fernando Stteffen <fernando.stteffen@gmail.com>
2022-02-02 18:11:31 -0300

    feat: add route to docs

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,28 @@
 import express, { NextFunction, Request, Response } from "express";
 import swaggerUi from "swagger-ui-express";
+
 import "express-async-errors";
+
+import "reflect-metadata";
+
+import { AppError } from "@errors/AppError";
+
+import { router } from "./routes";
+import swaggerFile from "./swagger.json";
 
 import "./database";
 
 import "@shared/container";
-
-import { AppError } from "@errors/AppError";
 
 const PORT = 3000;
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+app.use(router);
 
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
