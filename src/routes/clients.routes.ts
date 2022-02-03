5f76c450f08b1e5313bf80c2b5cbff52caa4509c
Fernando Stteffen <fernando.stteffen@gmail.com>
2022-02-03 03:04:50 -0300

    feat: add alter name routes

--- src/routes/clients.routes.ts
+++ src/routes/clients.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 
+import { AlterClientNameController } from "@modules/clients/useCases/alterClientName/AlterClientNameController";
 import { CreateClientController } from "@modules/clients/useCases/createClient/CreateClientController";
 import { DeleteClientController } from "@modules/clients/useCases/deleteClient/DeleteClientController";
 import { GetClientByIdController } from "@modules/clients/useCases/getClientByID/GetClientByIdController";
@@ -11,10 +12,12 @@ const createClientController = new CreateClientController();
 const listClientsController = new ListClientsController();
 const getClientByIdController = new GetClientByIdController();
 const deleteClientController = new DeleteClientController();
+const alterClientNameController = new AlterClientNameController();
 
 clientsRoutes.post("/", createClientController.handle);
 clientsRoutes.get("/", listClientsController.handle);
 clientsRoutes.get("/:id", getClientByIdController.handle);
 clientsRoutes.delete("/:id", deleteClientController.handle);
+clientsRoutes.patch("/:id", alterClientNameController.handle);
 
 export { clientsRoutes };
