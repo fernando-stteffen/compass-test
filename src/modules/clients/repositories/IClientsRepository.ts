995397c01ecf876c6091687fd6ef49a25699f7da
Fernando Stteffen <fernando.stteffen@gmail.com>
2022-02-03 00:43:25 -0300

    fix: clients repository interface

--- src/modules/clients/repositories/IClientsRepository.ts
+++ src/modules/clients/repositories/IClientsRepository.ts
@@ -12,14 +12,16 @@ interface IClientDTO {
   sex: string;
   birthDate: Date;
   city: string;
+  state: string;
+  age: number;
 }
 
 interface IClientsRepository {
-  findByName(name: string): Promise<Client>;
+  findByName(name: string): Promise<Client[]>;
   findById(id: string): Promise<Client>;
-  create({ name, sex, birthDate, city }: IClientDTO);
+  create({ name, sex, birthDate, city, state, age }: IClientDTO);
   delete(id: string): Promise<void>;
   alterName(id: string, name: string): Promise<void>;
 }
 
-export { IClientsRepository };
+export { IClientsRepository, IClientDTO };
