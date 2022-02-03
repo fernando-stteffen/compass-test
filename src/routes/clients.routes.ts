516494ba14526eebe5616a4f5f4e1eb26b568d77
Fernando Stteffen <fernando.stteffen@gmail.com>
2022-02-03 01:13:42 -0300

    feat: add route to list clients

--- src/routes/clients.routes.ts
+++ src/routes/clients.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 
 import { CreateClientController } from "@modules/clients/useCases/createClient/CreateClientController";
+import { ListClientsController } from "@modules/clients/useCases/listClients/ListClientsController";
 
 const clientsRoutes = Router();
 
-const createClientControlelr = new CreateClientController();
+const createClientController = new CreateClientController();
+const listClientsController = new ListClientsController();
 
-clientsRoutes.post("/", createClientControlelr.handle);
+clientsRoutes.post("/", createClientController.handle);
+clientsRoutes.get("/", listClientsController.handle);
 
 export { clientsRoutes };
