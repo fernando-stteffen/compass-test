00296cd05ac62d17fa14594319dcc89c69c1dd5a
Fernando Stteffen <fernando.stteffen@gmail.com>
2022-02-03 01:55:08 -0300

    feat: add clients/:id route to clients routes

--- src/routes/clients.routes.ts
+++ src/routes/clients.routes.ts
@@ -1,14 +1,17 @@
 import { Router } from "express";
 
 import { CreateClientController } from "@modules/clients/useCases/createClient/CreateClientController";
+import { GetClientByIdController } from "@modules/clients/useCases/getClientByID/GetClientByIdController";
 import { ListClientsController } from "@modules/clients/useCases/listClients/ListClientsController";
 
 const clientsRoutes = Router();
 
 const createClientController = new CreateClientController();
 const listClientsController = new ListClientsController();
+const getClientByIdController = new GetClientByIdController();
 
 clientsRoutes.post("/", createClientController.handle);
 clientsRoutes.get("/", listClientsController.handle);
+clientsRoutes.get("/:id", getClientByIdController.handle);
 
 export { clientsRoutes };
