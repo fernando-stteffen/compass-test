ed615f8650cd396cfa481257b47b3ff14236526e
Fernando Stteffen <fernando.stteffen@gmail.com>
2022-02-03 00:45:24 -0300

    feat: imported clients router to routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import { citiesRoutes } from "./cities.routes";
+import { clientsRoutes } from "./clients.routes";
 
 const router = Router();
 
 router.use("/cities", citiesRoutes);
+router.use("/clients", clientsRoutes);
 
 export { router };
